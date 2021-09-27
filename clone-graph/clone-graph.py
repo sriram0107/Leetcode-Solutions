e1e6c0d4b59b9f92f07490e71df4fd52db798489
Sriram Sathish <63444773+sriram0107@users.noreply.github.com>
2021-09-27 19:36:05 -0400

    Time: 40 ms (62.76%), Space: 14.6 MB (82.51%) - LeetHub

--- clone-graph/clone-graph.py
+++ clone-graph/clone-graph.py
@@ -6,29 +6,63 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
+'''
+undirected graph
+
+1 - 2 - 5
+|   |
+3 - 4
+
+(1)
+
+app 1) recursive and take care of duplication
+app 2) iterative -> bfs & store
+
+[(3)]
+(4)
+{
+    1: (1) [2,4]
+    2: (2) [3,1]
+    4: (4) [1,3]
+    3: (3) [2,4]
+    
+
+}
+'''
+from collections import deque
 
 class Solution:
+    def getClone(self, val):
+        clone = Node()
+        clone.val = val
+        clone.neighbors = []
+        return clone
+    
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node:
             return None
-        q = deque()
-        q.append(node)
+        
+        dq = deque()
         nodeMap = {}
-        visited = []
-        while len(q) != 0:
-            nd = q.popleft()
-            if nd.val not in nodeMap.keys():
-                nodeMap[nd.val] = Node()
-                nodeMap[nd.val].val = nd.val
-            visited.append(nd.val)
-            for nbr in nd.neighbors:
-                if nbr.val not in nodeMap.keys():
-                    q.append(nbr)
-                    nodeMap[nbr.val] = Node()
-                    nodeMap[nbr.val].val = nbr.val
-                nodeMap[nd.val].neighbors.append(nodeMap[nbr.val])   
-        return nodeMap[node.val]
+        
+        dq.append(node)
+        
+        while len(dq) != 0:
+            newNode = dq.popleft()
+            if newNode.val not in nodeMap.keys():
+                clone = self.getClone(newNode.val)
+                nodeMap[clone.val] = clone
             
+            for nbr in newNode.neighbors:
+                if nbr.val not in nodeMap.keys():
+                    nbrClone = self.getClone(nbr.val)
+                    nodeMap[nbrClone.val] = nbrClone
+                    nodeMap[newNode.val].neighbors.append(nbrClone)
+                    dq.append(nbr)       
+                else:
+                    nodeMap[newNode.val].neighbors.append(nodeMap[nbr.val])
+
+        return nodeMap[node.val]
         
         
         
